b33287b5eded6077eb2a94853ec8ce80d7d50561
rlaqhalx <rlaqhalx@gmail.com>
2023-05-16 19:45:00 -0400

    sign-in with firebase utils and libraries

---
+++ e-com-platform/src/utils/firebase/firebase.utils.js
@@ -0,0 +1,57 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth'
+import {getFirestore, doc, getDoc, setDoc} from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB04hqk9KJa1TqPXg4Ueg5w_3JB7UVFhZ4",
+  authDomain: "e-com-platform-db.firebaseapp.com",
+  projectId: "e-com-platform-db",
+  storageBucket: "e-com-platform-db.appspot.com",
+  messagingSenderId: "1004469873284",
+  appId: "1:1004469873284:web:cae52600d3a0710ff330d1"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+    prompt: "select_account"
+})
+
+export const auth = getAuth();
+// export const signInWithGooglePopup = () => {
+//     signInWithPopup(auth, provider);
+// }
+
+export const signInWithGooglePopup = () => signInWithPopup(auth,provider); 
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async(userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+    console.log(userDocRef)
+    const userSnapShot = await getDoc(userDocRef)
+    console.log(userSnapShot)
+    console.log(userSnapShot.exists()) // does it exist in db? -> t/f
+
+    // if user data does not exists
+    // create / set the document ith the data from userAuth in my collection
+
+    if (!userSnapShot.exists()) {
+        const {displayName, email} =userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName, email, createdAt
+            });
+        } catch (error) {
+            console.log('error creating the user', error.message)
+        }
+    }
+    // return userDocRef
+    return userDocRef;
+}
