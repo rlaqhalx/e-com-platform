c60af77925397c623c16ac1d623feee59c2bd722
rlaqhalx <rlaqhalx@gmail.com>
2023-06-23 17:07:35 -0400

    added stripe for payment & netlify cli

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,14 +1,18 @@
-import { useContext } from 'react';
-import { UseSelector, useSelector } from 'react-redux/es/hooks/useSelector';
-import { selectCartItems, selectCartTotal } from '../../store/cart/cart.selector';
-import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import { useContext } from "react";
+import { UseSelector, useSelector } from "react-redux/es/hooks/useSelector";
+import {
+  selectCartItems,
+  selectCartTotal,
+} from "../../store/cart/cart.selector";
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import PaymentForm from "../../components/payment-form/payment-form.component";
 
 import {
   CheckoutContainer,
   CheckoutHeader,
   HeaderBlock,
   Total,
-} from './checkout.styles';
+} from "./checkout.styles";
 
 const Checkout = () => {
   //const { cartItems, cartTotal } = useContext(CartContext);
@@ -38,6 +42,7 @@ const Checkout = () => {
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
       <Total>Total: ${cartTotal}</Total>
+      <PaymentForm />
     </CheckoutContainer>
   );
 };
